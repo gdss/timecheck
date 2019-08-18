299f88daa25d566df6d1ade3b1fc91490bee3f75
Geyson <geysondaniel@gmail.com>
2019-08-18 16:56:10 -0300

    Create spring boot project

---
+++ src/main/java/com/gdss/timecheck/TimecheckApplication.java
@@ -0,0 +1,13 @@
+package com.gdss.timecheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimecheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimecheckApplication.class, args);
+	}
+
+}
